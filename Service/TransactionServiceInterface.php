7cb7f4ce03e8cfac19b4c6cc0d41ebd21ce78d75
Alexander Kabanov <alexk@elrise.ru>
2025-04-01 14:13:17 +0800

    Add Transaction service

---
+++ Service/TransactionServiceInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ITech\DbalBundle\Service;
+
+use Closure;
+use Doctrine\DBAL\TransactionIsolationLevel;
+
+interface TransactionServiceInterface
+{
+    public function transactional(Closure $callback, ?TransactionIsolationLevel $isolation = null): mixed;
+
+    public function begin(?TransactionIsolationLevel $isolation = null): void;
+
+    public function commit(): void;
+
+    public function rollback(): void;
+
+    public function inTransaction(): bool;
+}
