013b179da73c2eebaa10ef7967e1cd579bdff5b2
Alexander Kabanov <alexk@elrise.ru>
2025-04-14 13:55:35 +0800

    Update Manager Factory + README

--- src/Manager/DbalManagerFactory.php
+++ src/Manager/DbalManagerFactory.php
@@ -16,45 +16,106 @@ use ITech\Bundle\DbalBundle\Manager\Iterator\OffsetIterator;
 use ITech\Bundle\DbalBundle\Manager\Mutator\DbalMutator;
 use ITech\Bundle\DbalBundle\Service\Serialize\DtoDeserializerInterface;
 use ITech\Bundle\DbalBundle\Sql\Builder\SqlBuilderFactory;
+use ITech\Bundle\DbalBundle\Sql\Builder\SqlBuilderInterface;
 
 final readonly class DbalManagerFactory
 {
     public function __construct(
-        private Connection $connection,
+        private Connection $defaultConnection,
         private DtoDeserializerInterface $deserializer,
-        private ?DbalBundleConfig $config = null,
+        private ?DbalBundleConfig $defaultConfig = null,
     ) {
+    }
+
+    private function getConfig(?DbalBundleConfig $config): DbalBundleConfig
+    {
+        return $config ?? $this->defaultConfig ?? new DbalBundleConfig();
+    }
+
+    public function createFinder(?Connection $connection = null): DbalFinder
+    {
+        return new DbalFinder(
+            $connection ?? $this->defaultConnection,
+            $this->deserializer
+        );
+    }
+
+    public function createMutator(?Connection $connection = null): DbalMutator
+    {
+        return new DbalMutator($connection ?? $this->defaultConnection);
+    }
+
+    public function createCursorIterator(?Connection $connection = null, ?DbalBundleConfig $config = null): CursorIterator
+    {
+        return new CursorIterator(
+            $connection ?? $this->defaultConnection,
+            $this->deserializer,
+            $this->getConfig($config)
+        );
+    }
+
+    public function createOffsetIterator(?Connection $connection = null, ?DbalBundleConfig $config = null): OffsetIterator
+    {
+        return new OffsetIterator(
+            $connection ?? $this->defaultConnection,
+            $this->deserializer,
+            $this->getConfig($config)
+        );
     }
 
     /**
      * @throws Exception
      */
-    public function create(): DbalManager
+    public function createSqlBuilder(Connection $connection, ?DbalBundleConfig $config = null): SqlBuilderInterface
     {
-        $config = $this->config ?? new DbalBundleConfig();
-
-        $sqlBuilder = (new SqlBuilderFactory(
-            $this->connection,
-            $config->placeholderStrategy,
+        return (new SqlBuilderFactory(
+            $connection,
+            $this->getConfig($config)->placeholderStrategy,
         ))->create();
+    }
 
-        $finder = new DbalFinder($this->connection, $this->deserializer);
-        $mutator = new DbalMutator($this->connection);
-        $cursorIterator = new CursorIterator($this->connection, $this->deserializer, $config);
-        $offsetIterator = new OffsetIterator($this->connection, $this->deserializer, $config);
+    /**
+     * @throws Exception
+     */
+    public function createBulkInserter(?Connection $connection = null, ?DbalBundleConfig $config = null): BulkInserter
+    {
+        $conn = $connection ?? $this->defaultConnection;
+        return new BulkInserter($conn, $this->createSqlBuilder($conn, $config), $this->getConfig($config));
+    }
 
-        $bulkInserter = new BulkInserter($this->connection, $sqlBuilder, $config);
-        $bulkUpdater = new BulkUpdater($this->connection, $sqlBuilder, $config);
-        $bulkUpserter = new BulkUpserter($this->connection, $sqlBuilder, $config);
+    /**
+     * @throws Exception
+     */
+    public function createBulkUpdater(?Connection $connection = null, ?DbalBundleConfig $config = null): BulkUpdater
+    {
+        $conn = $connection ?? $this->defaultConnection;
+        return new BulkUpdater($conn, $this->createSqlBuilder($conn, $config), $this->getConfig($config));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function createBulkUpserter(?Connection $connection = null, ?DbalBundleConfig $config = null): BulkUpserter
+    {
+        $conn = $connection ?? $this->defaultConnection;
+        return new BulkUpserter($conn, $this->createSqlBuilder($conn, $config), $this->getConfig($config));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function createManager(?Connection $connection = null, ?DbalBundleConfig $config = null): DbalManager
+    {
+        $conn = $connection ?? $this->defaultConnection;
 
         return new DbalManager(
-            $finder,
-            $mutator,
-            $cursorIterator,
-            $offsetIterator,
-            $bulkInserter,
-            $bulkUpdater,
-            $bulkUpserter,
+            $this->createFinder($conn),
+            $this->createMutator($conn),
+            $this->createCursorIterator($conn, $config),
+            $this->createOffsetIterator($conn, $config),
+            $this->createBulkInserter($conn, $config),
+            $this->createBulkUpdater($conn, $config),
+            $this->createBulkUpserter($conn, $config),
         );
     }
 }
