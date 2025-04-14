c67926410f0dd321c9cbfa35ce82931e64968c98
Alexander Kabanov <alexk@elrise.ru>
2025-04-14 12:33:08 +0800

    Add soft deletion + extra docs

--- src/Manager/Bulk/BulkDeleter.php
+++ src/Manager/Bulk/BulkDeleter.php
@@ -1,14 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ITech\Bundle\DbalBundle\Manager\Bulk;
 
+use DateTimeImmutable;
 use Exception;
 use ITech\Bundle\DbalBundle\Config\BundleConfigurationInterface;
 use ITech\Bundle\DbalBundle\Manager\Contract\BulkDeleterInterface;
 
 class BulkDeleter extends AbstractDbalWriteExecutor implements BulkDeleterInterface
 {
-
     public function deleteOne(string $tableName, string|int $id): int
     {
         return $this->deleteMany($tableName, [$id]);
@@ -27,10 +29,46 @@ class BulkDeleter extends AbstractDbalWriteExecutor implements BulkDeleterInterf
 
         $idFieldName = $this->fieldNames[BundleConfigurationInterface::ID_NAME];
 
-        $paramsList = array_map(static fn($id) => [$idFieldName => $id], $ids);
+        $paramsList = array_map(static fn ($id) => [$idFieldName => $id], $ids);
 
         [$flatParams, $types] = $this->sqlBuilder->prepareBulkParameterLists($paramsList);
 
         return $this->executeSql($sql, $flatParams, $types);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function deleteSoftMany(string $tableName, array $ids): int
+    {
+        if (empty($ids)) {
+            return 0;
+        }
+
+        $idFieldName = $this->fieldNames[BundleConfigurationInterface::ID_NAME];
+        $deletedAtFieldName = $this->fieldNames[BundleConfigurationInterface::DELETED_AT_NAME];
+        $now = (new DateTimeImmutable())->format('Y-m-d H:i:s');
+
+        $paramsList = array_map(
+            static fn ($id) => [
+                $deletedAtFieldName => $now,
+                $idFieldName => $id,
+            ],
+            $ids,
+        );
+
+        $sql = $this->sqlBuilder->getUpdateBulkSql($tableName, $paramsList, [$idFieldName]);
+
+        [$flatParams, $types] = $this->sqlBuilder->prepareBulkParameterLists($paramsList, [$idFieldName]);
+
+        return $this->executeSql($sql, $flatParams, $types);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function deleteSoftOne(string $tableName, string|int $ids): int
+    {
+        return $this->deleteSoftMany($tableName, [$ids]);
     }
 }
--- src/Manager/Contract/BulkDeleterInterface.php
+++ src/Manager/Contract/BulkDeleterInterface.php
@@ -1,16 +1,48 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ITech\Bundle\DbalBundle\Manager\Contract;
 
 interface BulkDeleterInterface
 {
     /**
      * Удаляет одну запись из указанной таблицы по ID.
+     *
+     * @param string $tableName название таблицы
+     * @param string|int $id ID записи для удаления
+     *
+     * @return int количество удаленных записей
      */
-    public function deleteOne(string $tableName, string $id): int;
+    public function deleteOne(string $tableName, string|int $id): int;
 
     /**
      * Удаляет несколько записей по массиву ID.
+     *
+     * @param string $tableName название таблицы
+     * @param array $ids массив ID записей для удаления
+     *
+     * @return int количество удаленных записей
      */
     public function deleteMany(string $tableName, array $ids): int;
+
+    /**
+     * Удаляет несколько записей из указанной таблицы по массиву ID с использованием мягкого удаления.
+     *
+     * @param string $tableName название таблицы
+     * @param array $ids массив ID записей для удаления
+     *
+     * @return int количество удаленных записей
+     */
+    public function deleteSoftMany(string $tableName, array $ids): int;
+
+    /**
+     * Удаляет одну запись из указанной таблицы по ID с использованием мягкого удаления.
+     *
+     * @param string $tableName название таблицы
+     * @param string|int $ids ID записи для удаления
+     *
+     * @return int количество удаленных записей
+     */
+    public function deleteSoftOne(string $tableName, string|int $ids): int;
 }
